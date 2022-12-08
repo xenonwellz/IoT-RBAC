e5baa0dd0ac37b6a72f916c8e3eea00d29d8caba
Obed <xenonwellz@gmail.com>
2022-12-08 15:27:58 +0100

    added base url support and README.

--- views/components/footer.php
+++ views/components/footer.php
@@ -1,11 +1,11 @@
 </main>
 
-<script src="/js/jquery.js"></script>
-<script src="/js/web3.min.js"></script>
-<script src="/js/truffle-contract.min.js"></script>
-<script src="/js/web3.js"></script>
-<script src="/js/toastr.min.js"></script>
-<script src="/js/main.js"></script>
+<script src="<?= $base ?>js/jquery.js"></script>
+<script src="<?= $base ?>js/web3.min.js"></script>
+<script src="<?= $base ?>js/truffle-contract.min.js"></script>
+<script src="<?= $base ?>js/web3.js"></script>
+<script src="<?= $base ?>js/toastr.min.js"></script>
+<script src="<?= $base ?>js/main.js"></script>
 </body>
 
 </html>
--- views/components/header.php
+++ views/components/header.php
@@ -1,11 +1,12 @@
+<?php $base = '/'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="/css/bundle/app.css">
-    <link rel="stylesheet" href="/css/toastr.min.css">
+    <link rel="stylesheet" href="<?= $base ?>css/bundle/app.css">
+    <link rel="stylesheet" href="<?= $base ?>css/toastr.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <!-- <script src="/js/turbolinks.js"></script> -->
